b73aa0fe655739bfa5a67d0079e4712429df3fbe
Omlet144 <alexblack144@gmail.com>
2023-02-27 00:22:11 +0200

    All Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from "./pages/Main";
+import Regist from './pages/Regist';
+import Login from './pages/Login';
+import Manager from './pages/Manager';
+import RegistManager from './pages/RegistManager';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+       <Routes>
+         <Route path='/' element={<Main/>}></Route>
+         <Route path='/regist' element={<Regist/>}></Route>
+         <Route path='/login' element={<Login/>}></Route>
+         <Route path='/manager' element={<Manager/>}></Route>
+         <Route path='/registManager' element={<RegistManager/>}></Route>
+       </Routes>
+     </BrowserRouter>
   );
 }
 
---
+++ src/pages/Main.js
@@ -0,0 +1,180 @@
+import { Link, Outlet } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ReactLogo from '../logout_black_24dp.svg';
+import ReactLogo2 from '../person_outline_black_24dp.svg';
+import ReactLogo3 from '../create_black_24dp.svg';
+
+function Main(){
+
+    const [gadgets, setGadgets] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [flag, setFlag] = useState(0);
+    const [flag2, setFlag2] = useState(0);
+    
+    function getOut(){
+        if(window.sessionStorage.getItem('token')==null|| window.sessionStorage.getItem('token')=='null')
+        {
+            alert('You don`t login!');
+        }
+        else{
+            
+            console.log(window.sessionStorage.getItem('token'));
+            window.sessionStorage.setItem('token', null)
+            window.location.href = '/login';
+        }
+       
+    }
+    function getAllGadgets(){
+        
+        if(flag == 0)
+        {
+            axios({
+                method:'get',
+                url: "https://localhost:7020/Gadget/Get Gadgets",
+                headers: {
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(data=>
+            {
+                setGadgets(data.data);    
+            })
+            setFlag(1);
+            
+        }
+    }
+    function getGadgetsById(id)
+    {
+        console.log(id);
+        axios({
+            method:'get',
+            url: `https://localhost:7020/Gadget/Get Gadget by Id_Category?id=${id}`,
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(data=>
+        {
+            setGadgets(data.data);    
+        });
+        
+    }
+    function getCategorys()
+    {
+        if(flag2 == 0)
+        {
+            axios({
+                method:'get',
+                url: "https://localhost:7020/Category/Get Categorys",
+                headers: {
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(data=>
+            {
+                setCategories(data.data);
+            })
+            setFlag2(1);
+        }
+    }
+    function buyBtn()
+    {
+        if(window.sessionStorage.getItem('token')==null || window.sessionStorage.getItem('token')=='null')
+        {
+            alert("You need to Login!")
+            console.log(window.sessionStorage.getItem('token'));
+        }
+        else
+        {
+            console.log(window.sessionStorage.getItem('token'));
+            alert("Congratulations you Buy it!")
+        }
+    }
+    return(
+    <div className="App1">
+        <div className="App-header1">
+            
+            <div className="hamburger">
+                <div className="hamburgerMenu">
+                    <div><Link className="link" to="/regist">Regist</Link></div>
+                    <div><Link className="link" to="/login">Login</Link></div>
+                    <div><b className="link"  style={{ cursor: 'pointer'}} id="out" onClick={()=>getOut()}>Out</b></div> 
+
+                    {
+                        categories.map((item, index)=>(
+                            <div key={index} style={{ cursor: 'pointer'}} className = "link" id={item.id} onClick={() => getGadgetsById(item.id)}>
+                            {item.name}
+                            </div>
+                        ))
+                    }      
+                </div>
+            </div>
+          
+            <div id="DivRegisAndLoginLinks">
+                <div>
+                    <Link className="link" to="/regist">
+                        Regist  
+                        <img src={ReactLogo3} style={{width:'18px', height: '18px'}} alt="React Logo"></img>
+                    </Link>
+                    
+                    <b className="RegisAndLoginLinks" > or </b>
+                    <Link className="link" to="/login">
+                        Login   
+                        <img src={ReactLogo2} style={{width:'18px', height: '18px'}} alt="React Logo"></img>
+                    </Link>
+                    
+                </div>
+                <div onClick={()=>getOut()} style={{ cursor: 'pointer'}}>
+                    <b className="link" id="out" >Out   </b>
+                    <img src={ReactLogo} style={{width:'18px', height: '18px'}} alt="React Logo"></img>
+                </div>
+                
+            </div>
+            <br></br>
+            <div style={{textAlign: 'center'}}>
+                <span className="top" onClick={()=>(window.location.reload())}>STORE OF GADGETS</span>
+            </div>
+            <br></br>
+
+            <div className="container-menu" id="container" style={{cursor: 'pointer'}}>
+            {getCategorys()}
+            {console.log(categories)}
+            {
+                categories.map((item, index)=>(
+                    <div key={index}  className = "name_category" id={item.id} onClick={() => getGadgetsById(item.id)}>
+                       {item.name}
+                    </div>
+                ))
+            }
+            </div> 
+            
+            <br></br>
+
+            <div id="container-tovarov">
+            {getAllGadgets()}
+            {
+               gadgets.map((item, index)=>(
+                <div key={index} className="cart" id={item.id}>
+                    <img className="img_gadget" src={item.image}></img>
+                    <div className="cart_gadget">{item.name}</div>
+                    <div className="cart_gadget">{item.model}</div>
+                    <div className="cart_gadget">{item.price}₴</div>
+                    <button className="BuyBtn" onClick={buyBtn}>Buy</button>
+                </div>
+               ))  
+            }
+            </div>
+        </div>
+
+    </div>
+       
+        
+        
+    );
+}
+
+export default Main;
